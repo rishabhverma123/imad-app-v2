0b7ab18726e7260df30b8a1c5b1d42383693cf4d
rishabhverma123 <rishabhverma42@gmail.com>
2017-02-10 22:57:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -116,6 +116,11 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/article-one', function (req, res) {
+  res.sendFile(createTemplate(articleOne));
+});
+
+
 app.get('/:articleName', function (req, res) {
     
     var articleName=req.params.articleName;
