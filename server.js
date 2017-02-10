1f9de502cd01618d4935352535575151bb36819c
rishabhverma123 <rishabhverma42@gmail.com>
2017-02-10 22:57:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -120,13 +120,13 @@ app.get('/article-one', function (req, res) {
   res.sendFile(createTemplate(articleOne));
 });
 
-
+/*
 app.get('/:articleName', function (req, res) {
     
     var articleName=req.params.articleName;
   res.sendFile(createTemplate(articles[articleName]));
 });
-
+*/
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
